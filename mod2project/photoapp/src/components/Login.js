417f16ad36aa62c03c463cb26c630eb1b5ea52bd
aparna4scholas <aparna4scholas@gmail.com>
2020-04-23 03:30:20 -0500

    added

--- mod2project/photoapp/src/components/Login.js
+++ mod2project/photoapp/src/components/Login.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 function Login () {
     return (
-        <div>
+        <div className="login_d1">
         <label className="label_1"> Email</label>
         <input type="text" name="Email" placeholder=""/>
         </div>
