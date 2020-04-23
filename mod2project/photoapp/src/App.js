62758602394e5847c82e90d1d1fa2f0b99991c05
aparna4scholas <aparna4scholas@gmail.com>
2020-04-23 16:57:20 -0500

    modified

--- mod2project/photoapp/src/App.js
+++ mod2project/photoapp/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import './App.css';
+import './App.scss';
 import Navbar from './components/Navbar.js';
 import Home from './components/Home.js';
 import Login from './components/Login.js';
