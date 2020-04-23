fe000621894f5358ebcb30e14e88e685e0c2d934
aparna4scholas <aparna4scholas@gmail.com>
2020-04-23 02:00:03 -0500

    added

--- mod2project/photoapp/src/App.js
+++ mod2project/photoapp/src/App.js
@@ -17,8 +17,7 @@ export default class  extends Component {
           <Route path="/Home" component={Home} />  
           <Route path="/Login" component={Login} /> 
           <Route path="/Signup" component={Signup} /> 
-          <Login />
-          <Signup />
+          
           <Pictures />
 
            {/*<switch>
