b2d8ab8567250edfa898b96ed6763f61b952d53b
aparna4scholas <aparna4scholas@gmail.com>
2020-04-25 01:09:49 -0500

    modified

--- mod2project/photoapp/src/components/Login.js
+++ mod2project/photoapp/src/components/Login.js
@@ -4,12 +4,18 @@ function Login () {
     return (
         <div className="login_d1">
         <label className="label_1"> Email</label>
-        <input type="text" name="Email" placeholder=""/>
-        
+        <input className="input_1" type="text" name="Email" placeholder="email "/> 
+       
+      
+         <label className="label_2"> Password</label>
+        <input className="input_2" type="text" name="password" placeholder="password "/>
         </div>
+        <div> <button>
+            </button> 
+        </div>
+        
             )
-
-    }
+        }
     
 
 export default Login;
